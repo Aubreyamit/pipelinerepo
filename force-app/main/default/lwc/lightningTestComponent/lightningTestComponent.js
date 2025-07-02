14732eb2fc7cca10aef2b5b5cf288bc26a3729c8
Amit Kumar <aubreyamit2019@gmail.com>
2025-07-02 12:17:35 +0530

    This is fourth update

--- force-app/main/default/lwc/lightningTestComponent/lightningTestComponent.js
+++ force-app/main/default/lwc/lightningTestComponent/lightningTestComponent.js
@@ -3,4 +3,5 @@ export default class LightningTestComponent extends LightningElement {
     //This is a test js file 
     //This is another comment
     //This is third comment
+    //This is fourth comment
 }
